96b8df9ce0c5a5da991bc4715ec2608a0bb6a0d2
Iván L. Murillo <diskman199@gmail.com>
2019-05-02 23:18:59 -0500

    Soporte de imágenes
    Ya quedó bien chido el soporte de imágenes, solo falta hacer el 99.1% restante

--- src/cocofilosofospro/View.java
+++ src/cocofilosofospro/View.java
@@ -7,8 +7,15 @@ package cocofilosofospro;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
@@ -16,31 +23,71 @@ import javax.swing.JFrame;
  */
 public class View extends JFrame {
 
+    protected ImageIcon eating;
+    protected ImageIcon thinking;
+    protected ImageIcon waiting;
+    protected ImageIcon free;
+    protected ImageIcon using;
     protected int width;
     protected int height;
     protected JButton begin;
+    protected JLabel[] philosophers;
+    protected JLabel[] forks;
 
-    View() 
-    {
+    View() {
         width = 500;
         height = 500;
+
+        philosophers = new JLabel[5];
+        forks = new JLabel[5];
+
+        initImages();
         initWindow();
         initComponents();
+
         this.setLayout(null);
     }
-    
-    protected void initComponents()
+
+    protected void initImages() 
     {
-        return;
+        eating = getImage("https://www.dictionary.com/e/wp-content/uploads/2018/03/Thinking_Face_Emoji-Emoji-Island.png", 50, 50);
+        thinking = getImage("https://www.dictionary.com/e/wp-content/uploads/2018/03/Thinking_Face_Emoji-Emoji-Island.png", 50, 50);
+        waiting = getImage("https://www.dictionary.com/e/wp-content/uploads/2018/03/Thinking_Face_Emoji-Emoji-Island.png", 50, 50);
+
+        free = getImage("https://i2.wp.com/www.eatlikeabeast.com/wp-content/uploads/2016/06/cropped-fork-knife-emoji.png", 50, 50);
+        using = getImage("https://i2.wp.com/www.eatlikeabeast.com/wp-content/uploads/2016/06/cropped-fork-knife-emoji.png", 50, 50);
     }
 
-    protected void initWindow() 
-    {
+    protected ImageIcon getImage(String source, int w, int h) {
+        try {
+            ImageIcon icon = new ImageIcon(new URL(source));
+            Image resize = icon.getImage();
+            resize = resize.getScaledInstance(w, h, java.awt.Image.SCALE_SMOOTH);
+            return new ImageIcon(resize);
+        } catch (MalformedURLException ex) {
+            System.out.println("Error al obtener URL: " + ex.toString());
+            return null;
+        }
+    }
+
+    protected void initComponents() {
+        for (int i = 0; i < philosophers.length; i++) {
+            philosophers[i] = new JLabel(thinking);
+            philosophers[i].setBounds(10 + i * 60, 10, 50, 50);
+
+            //this.getContentPane().add(philosophers[i]);
+            this.add(philosophers[i]);
+
+            System.out.println("Icono agregado");
+        }
+    }
+
+    protected void initWindow() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(width, height);
         this.setTitle("Cena de filósofos");
 
-        Color color = new Color(102, 153, 153, 255);
+        Color color = new Color(255, 255, 255, 255);
         this.getContentPane().setBackground(color);
 
         begin = new JButton();
@@ -48,17 +95,16 @@ public class View extends JFrame {
         Font font = new Font("Arial", Font.PLAIN, 12);
         begin.setText("Empezar");
         begin.setFont(font);
-        
-        begin.addActionListener(e->
-        {
+
+        begin.addActionListener(e
+                -> {
             startEating();
         });
 
         this.add(begin);
     }
-    
-    protected void startEating()
-    {
+
+    protected void startEating() {
         System.out.println("Empezando...");
     }
 
